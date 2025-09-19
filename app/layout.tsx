2528d3a92ac3c9f76e0b4525b7533ad9e8daa617
Semmy <hello@cybrance.nl>
2025-09-19 13:58:07 +0200

    GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Albert_Sans } from "next/font/google";
 import "./globals.css";
 
 import { ReactNode } from "react";
-import ClientWrapper from "@/components/ClientWrapper"; // our client component
+import ClientWrapper from "@/components/ClientWrapper";
+import Script from "next/script";
 
 const albertSans = Albert_Sans({
   variable: "--font-albert-sans",
@@ -46,7 +49,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${albertSans.variable}`}>
       <body className="antialiased bg-white text-[var(--color-dark)] transition-colors duration-300">
-        {/* Wrapper with responsive horizontal padding */}
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0CS17B888C"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0CS17B888C');
+          `}
+        </Script>
+
         <div className="px-4 md:px-8">
           <ClientWrapper>{children}</ClientWrapper>
         </div>
