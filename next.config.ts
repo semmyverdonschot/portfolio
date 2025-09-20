852120d72355b5376139364a9448f2b58b3a26dd
Semmy <hello@cybrance.nl>
2025-09-20 20:02:47 +0200

    next.conf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  swcMinify: true,
   experimental: {
     optimizeCss: true,
     esmExternals: true,
@@ -12,6 +11,10 @@ const nextConfig: NextConfig = {
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
+    reactRemoveProperties: process.env.NODE_ENV === "production",
+  },
+  future: {
+    strictPostcssConfiguration: true,
   },
 };
 
