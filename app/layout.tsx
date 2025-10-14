d16b1a317e7050bdc4749f440918b3f920581d64
Semmy <hello@cybrance.nl>
2025-10-14 14:02:40 +0200

    Removed gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { ReactLenis } from "lenis/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
@@ -22,19 +21,6 @@ export default function RootLayout({
     <html lang="en">
       <ReactLenis root>
         <body className={albertSans.variable}>
-          {/* Google Analytics */}
-          <Script
-            strategy="afterInteractive"
-            src="https://www.googletagmanager.com/gtag/js?id=G-0CS17B888C"
-          />
-          <Script id="gtag-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0CS17B888C', { send_page_view: false });
-            `}
-          </Script>
           <DocumentTitleChanger />
 
           <div className="px-4 md:px-8" style={{ overflow: "visible" }}>
